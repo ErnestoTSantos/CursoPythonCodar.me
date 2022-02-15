4a14fb9ac129d2aa4ef3e6ce6d793c2490b5c412
ErnestoTSantos <ernesto.terra2003@gmail.com>
2022-02-15 10:39:50 -0300

    Realização do exercício sem if/else.

--- Operadores/Exercicios_Operadores/exercicio_4.py
+++ Operadores/Exercicios_Operadores/exercicio_4.py
@@ -2,9 +2,8 @@ buy_value = float(input('Digite o valor da compra: '))
 cost_freight = float(input('Digite o valor do frete: '))
 registered = input('O cliente é cadastrado? (S/N): ').upper()
 
-if buy_value + cost_freight > 100:
-    print('O desconto pode ser utilizado!')
-elif registered == 'S':
-    print('O desconto pode ser utilizado!')
-else:
-    print('O desconto não pode ser aplicado!')
+verifyPurchasesValues = buy_value + cost_freight >= 100
+verifyRegister = registered == 'S'
+verifyResult = verifyPurchasesValues or verifyRegister
+
+print(f'O desconto poderá ser aplicado: {verifyResult}')
