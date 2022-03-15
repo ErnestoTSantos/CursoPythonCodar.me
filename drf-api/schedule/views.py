c852b075eafb0361e8cf280aa6c1e8db43fb6921
ErnestoTSantos <ernesto.terra2003@gmail.com>
2022-03-15 11:03:29 -0300

    Adição do método POST

--- drf-api/schedule/views.py
+++ drf-api/schedule/views.py
@@ -7,12 +7,28 @@ from schedule.serializers import SchedulingSerializer
 from .models import Scheduling
 
 
-@api_view(http_method_names=['GET'])
+@api_view(http_method_names=['GET', 'POST'])
 def scheduling_list(request):
-    qs = Scheduling.objects.all()
-    serializer = SchedulingSerializer(qs, many=True)
+    if request.method == 'GET':
+        qs = Scheduling.objects.all()
+        serializer = SchedulingSerializer(qs, many=True)
 
-    return JsonResponse(serializer.data, safe=False)
+        return JsonResponse(serializer.data, safe=False)
+
+    if request.method == 'POST':
+        data = request.data
+        serializer = SchedulingSerializer(data=data)
+
+        if serializer.is_valid():
+            validated_data = serializer.validated_data
+            Scheduling.objects.create(
+                date_time=validated_data['date_time'],
+                client_name=validated_data['client_name'],
+                client_email=validated_data['client_email'],
+                client_phone=validated_data['client_phone'],
+            )
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
 
 # Aqui a view irá ser vista como uma api e sempre retornará elementos json.
