4cc764e7349707c8f2dc8b143afb67a0706bdce1
ErnestoTSantos <ernesto.terra2003@gmail.com>
2022-04-27 01:51:31 -0300

    Modificação nos testes e no gitignore, adição da última aula

--- drf-api/schedule/tests/test_send_mail.py
+++ drf-api/schedule/tests/test_send_mail.py
@@ -1,49 +1,34 @@
 from django.core import mail
 from django.test import TestCase
+from schedule.tasks import generate_report_provider
 
 
 class EmailTest(TestCase):
-    def test_send_email(self):
-        mail.send_mail(
-            "Subject here",
-            "Here is the message.",
-            "from@example.com",
-            ["to@example.com"],
-            fail_silently=False,
-        )
+    def test_send_mail(self):
+        generate_report_provider()
 
-        assert len(mail.outbox) == 1
-        assert mail.outbox[0].subject == "Subject here"
-
-    def test_sender_email(self):
-        mail.send_mail(
-            "Subject here",
-            "Here is the message.",
-            "from@example.com",
-            ["to@example.com"],
-            fail_silently=False,
-        )
-
-        assert mail.outbox[0].from_email == "from@example.com"
+        assert mail.outbox[0].subject == "Marked -> Provider report"
 
     def test_receiver_email(self):
-        mail.send_mail(
-            "Subject here",
-            "Here is the message.",
-            "from@example.com",
-            ["to@example.com"],
-            fail_silently=False,
-        )
+        generate_report_provider()
 
-        assert mail.outbox[0].to[0] == "to@example.com"
+        assert mail.outbox[0].to[0] == "ernesto.terra2003@gmail.com"
 
     def test_have_attachment(self):
-        mail.send_mail(
-            "Subject here",
-            "Here is the message.",
-            "from@example.com",
-            ["to@example.com"],
-            fail_silently=False,
-        )
+        generate_report_provider()
 
-        assert mail.outbox[0].attachments == []
+        assert mail.outbox[0].attachments
+
+    def test_attachment_has_expected_values(self):
+        generate_report_provider()
+
+        attachments = mail.outbox[0].attachments[0]
+
+        name, headers, type = attachments
+
+        assert name == "report.csv"
+        assert (
+            headers
+            == "provider,client_name,client_email,client_phone,confirmed,states\r\n"
+        )
+        assert type == "text/csv"
